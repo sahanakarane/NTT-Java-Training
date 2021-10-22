c5f7d6c168b5346c85c1b0067b1ce9c35e7fe2d0
sahanakarane <61207881+sahanakarane@users.noreply.github.com>
2021-10-22 19:10:14 +0530

    Create Honda.java

---
+++ associationexample1/Honda.java
@@ -0,0 +1,13 @@
+package com.association;
+
+public class Honda extends Car{
+
+	public void hondaStartEngine() {
+		CarEngine hondaEngine = new CarEngine();
+		hondaEngine.engineStarts();
+	}
+	public void hondaMusicPlayer() {
+		MusicPlayer hondaPlayer=new MusicPlayer();
+		hondaPlayer.carMusicPlayer();
+	}
+}
