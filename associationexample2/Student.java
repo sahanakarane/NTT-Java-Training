194611ac9b64f5c88b51d4984fba73438839b17e
sahanakarane <61207881+sahanakarane@users.noreply.github.com>
2021-10-22 19:14:25 +0530

    Create Student.java

---
+++ associationexample2/Student.java
@@ -0,0 +1,26 @@
+package com.associationexample;
+
+public class Student {
+ private String name;
+ private String rollno;
+ private String city;
+ public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getRollno() {
+	return rollno;
+}
+public void setRollno(String rollno) {
+	this.rollno = rollno;
+}
+public String getCity() {
+	return city;
+}
+public void setCity(String city) {
+	this.city = city;
+}
+
+}
